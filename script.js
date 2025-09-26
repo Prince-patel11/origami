833b17363b9320ac82b3ed86dab22232567677a7
Prince-patel11 <sagrittusa@gmail.com>
2025-09-26 15:34:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function alert_img(){
+alert("this is an origami image");
+document.getElementById("image").src="https://images.unsplash.com/photo-1709023739282-df32ed071a4a?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NTcxNjAyNDB8&ixlib=rb-4.1.0&q=85";
+}
